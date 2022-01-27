1d9f1511e8dc9744bfa7b790f6f8a0b072ae4d5d
Konstantin <kovucholegich@mail.ru>
2022-01-27 20:08:28 +0300

    Django_project

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
 
@@ -9,3 +9,10 @@ def show_all_movie(request):
     movies = Movie.objects.all()
     return render(request, 'movie_app/all_movies.html', {
         'movies_key': movies})
+
+
+def show_one_movie(request, id_movie: int):
+    # movie = Movie.objects.get(id=id_movie) - Если не найдёт введёный пользователем индекс,то будет ошибка DoesNotExist
+    movie = get_object_or_404(Movie, id=id_movie)  # Если не найдёт введёный пользователем индекс,то будет ошибка pnf404
+    return render(request, 'movie_app/one_movie.html', {
+        'movie_key': movie})
