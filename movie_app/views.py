30ab0d2f8b0725cee0ba03042085e623e74f2a82
Konstantin <kovucholegich@mail.ru>
2022-01-30 13:19:24 +0300

    Django_project

--- movie_app/views.py
+++ movie_app/views.py
@@ -6,12 +6,13 @@ from .models import Movie
 
 
 def show_all_movie(request):
-    movies = Movie.objects.all()
+    # movies = Movie.objects.all()  # Берём все объекты
+    movies = Movie.objects.order_by('-rating', 'budget')
     return render(request, 'movie_app/all_movies.html', {
         'movies_key': movies})
 
 
 def show_one_movie(request, slug_movie: str):
-    # movie = Movie.objects.get(id=id_movie) - Если не найдёт введёный пользователем индекс,то будет ошибка DoesNotExist
-    movie = get_object_or_404(Movie, slug=slug_movie)  # Если не найдёт введёный пользователем индекс,то будет ошибка pnf404
+    # movie = Movie.objects.get(id=id_movie) - Если не найдёт введёный пользователем индекс (request, id_movie: int),то будет ошибка DoesNotExist
+    movie = get_object_or_404(Movie, slug=slug_movie)  # Если не найдёт введёный пользователем slug,то будет ошибка pnf404
     return render(request, 'movie_app/one_movie.html', {'movie_key': movie})
