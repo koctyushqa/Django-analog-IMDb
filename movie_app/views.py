1fba413914ee83e1e651f2ecbf0c7cd0995840ab
Konstantin <kovucholegich@mail.ru>
2022-01-30 15:58:51 +0300

    Django_project

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
-
+from django.db.models import F, Max, Min, Sum, Count, Avg
 
 # Create your views here.
 
@@ -8,8 +8,12 @@ from .models import Movie
 def show_all_movie(request):
     # movies = Movie.objects.all()  # Берём все объекты
     movies = Movie.objects.order_by('-rating', 'budget')
+    # movies = Movie.objects.order_by(F('year').asc(null_last=True), '-rating') # Сортировка для null через F
+    agg = movies.aggregate(Avg("budget"), Max("rating"), Min("rating"), Count("id"))
     return render(request, 'movie_app/all_movies.html', {
-        'movies_key': movies})
+        'movies_key': movies,
+        'agg_key': agg,
+    })
 
 
 def show_one_movie(request, slug_movie: str):
