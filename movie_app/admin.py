3a2df17775cf9367c19625a0d5d5422fedd09285
Konstantin <kovucholegich@mail.ru>
2022-02-01 17:17:09 +0300

    Django_project

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Movie
 
+
 # Register your models here.
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget']
+    list_editable = ['rating', 'year', 'budget']
+    ordering = ['-rating', '-name']
+    list_per_page = 5
+
+admin.site.register(Movie, MovieAdmin)
--- movie_app/models.py
+++ movie_app/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 
+
 # Create your models here.
 
 class Movie(models.Model):
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
-    year = models.IntegerField(null=True)
+    year = models.IntegerField(null=True, blank=True)
     budget = models.IntegerField(default=1000000)
-    slug = models.SlugField(default='', null=False)
+    slug = models.SlugField(default='', null=False, db_index=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
