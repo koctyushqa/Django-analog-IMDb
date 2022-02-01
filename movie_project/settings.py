7460de17909f17089015ec94733865b3f7672a2d
Konstantin <kovucholegich@mail.ru>
2022-02-01 16:17:24 +0300

    Django_project

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movie_app',
-    'django_extensions'
-    'debug_toolbar'
+    'django_extensions',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
