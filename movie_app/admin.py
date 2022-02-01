9e1dd38f105302dc1b71a4d034b5319bc53552c7
Konstantin <kovucholegich@mail.ru>
2022-02-01 17:41:32 +0300

    Django_project

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -4,10 +4,20 @@ from .models import Movie
 
 # Register your models here.
 
+@admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget']
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
     list_editable = ['rating', 'year', 'budget']
     ordering = ['-rating', '-name']
-    list_per_page = 5
+    list_per_page = 10
 
-admin.site.register(Movie, MovieAdmin)
+    @admin.display(ordering='rating', description='Оценка зрителей')
+    def rating_status(self, mov: Movie):
+        if mov.rating < 50:
+            return 'Зря потратишь время!'
+        if mov.rating < 70:
+            return 'Разок можно глянуть.'
+        if mov.rating <= 85:
+            return 'Приятного просмотра.'
+        if mov.rating <= 100:
+            return 'Шедевр!'
